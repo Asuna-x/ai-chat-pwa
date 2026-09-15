68e18d6a0a68e65d07bcdecaeb418fb76ab4d7e9
Asuna-x <asuna8861@gmail.com>
2026-09-16 01:06:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "iris-shell-v20260915-43";
+const CACHE_NAME = "iris-shell-v20260915-44-43";
 
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil((async () => { const keys = await caches.keys(); await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))); await self.clients.claim(); })()); });
